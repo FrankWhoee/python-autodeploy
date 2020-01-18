367f93e20995def47dc75e17b69dc3a468e4d317
Frank Hui <huifrank@live.com>
2020-01-18 13:11:24 -0800

    Kills app on exception

--- autodeployer.py
+++ autodeployer.py
@@ -1,9 +1,32 @@
 from util import github
 import time
+import subprocess
+import yaml
 
 github.pull_repo()
+config = yaml.load(open("autodeploy.conf"))
+
+p = subprocess.Popen("exec " + "python " + config['run'], stdout=subprocess.PIPE, shell=True)
+
+
+def restart_app():
+    global p
+    p.kill()
+    p = subprocess.Popen("exec " + "python " + config['run'], stdout=subprocess.PIPE, shell=True)
+
+
+def kill_app():
+    global p
+    p.kill()
+
 
 while True:
-    if github.pull_repo():
-        print("New commit found. Repository updated.")
-    time.sleep(300)
+    try:
+        if github.pull_repo():
+            print("autodeploy[ " + config['repo'] + "]: New commit found. Repository updated.")
+            print("autodeploy[ " + config['repo'] + "]: Restarting app...")
+            restart_app()
+            print("autodeploy[ " + config['repo'] + "]: App is running...")
+        time.sleep(300)
+    except:
+        kill_app()
--- util/github.py
+++ util/github.py
@@ -1,19 +1,24 @@
-import requests
-from util import session
 import json
+
+import requests
+import yaml
 from git import Repo
+
+from util import session
 
 # Loads previous commit SHA if it exists within Session
 sha = session.get("sha")
 
-repo = Repo(".")
+repo = Repo("")
 assert not repo.bare
 
 origin = repo.remote("origin")
-
+api_url = origin.refs
+config = yaml.load(open("autodeploy.conf"))
+repo_name = config['repo']
 
 def has_new_update():
-    response = requests.get('https://api.github.com/repos/vikingsdev/dentaku/commits')
+    response = requests.get('https://api.github.com/repos/'+repo_name+'/commits')
     data = json.loads(response.text)[0]
     return data["sha"] != sha, data["sha"]
 
