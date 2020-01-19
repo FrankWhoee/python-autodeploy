ebd76de16757d9242de926ccc59006e8e7c87dc3
Frank Hui <huifrank@live.com>
2020-01-19 13:57:38 -0800

    Add custom shell source

--- autodeployer.py
+++ autodeployer.py
@@ -1,21 +1,27 @@
 from util import session
 from util import github
 import time
-import subprocess
+import subprocess, os
 import yaml
 from signal import signal, SIGINT
 from sys import exit
 
 p = None
 
+def shell_source(script):
+    """Sometime you want to emulate the action of "source" in bash,
+    settings some environment variables. Here is a way to do it."""
+    pipe = subprocess.Popen(". %s; env" % script, stdout=subprocess.PIPE, shell=True)
+    output = pipe.communicate()[0]
+    env = dict((line.split("=", 1) for line in output.splitlines()))
+    os.environ.update(env)
 
 def start_app():
     global p
     p = subprocess.Popen("exec " + "pip install -r requirements.txt", stdout=subprocess.PIPE, shell=True)
     p = subprocess.Popen("exec " + "python " + config['run'], stdout=subprocess.PIPE, shell=True)
     if 'export' in config:
-        p = subprocess.Popen("exec " + "source " + config['export'], stdout=subprocess.PIPE, shell=True)
-
+        shell_source(config['export'])
 
 def restart_app():
     kill_app()
